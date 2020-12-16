25fb4cbba3ebd20597dee9d758058c7d5f155fee
Ilari Lehtinen <ilari.m.lehtinen@gmail.com>
2020-12-16 12:57:48 +0200

    uusi harjopitus

---
+++ JObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class JObject
+    {
+        internal static JObject Parse(object json)
+        {
+            throw new NotImplementedException();
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
 
 namespace ConsoleApp2
 {
     class Program
     {
+        public static object JsonConvert { get; private set; }
+
         static void Main(string[] args)
         {
-            int counter = 0;
-            int summa = 0;
-            string rivi;
-            
-            System.IO.StreamReader file = new System.IO.StreamReader("test.txt");
 
-            while ((rivi = file.ReadLine()) != null)
+            WebRequest request = WebRequest.Create(@"https://api.openweathermap.org/data/2.5/weather?q=tampere&units=metric&appid=ba13b5feb3f961ed23f5195a6decb4e5");
+            WebResponse response = request.GetResponse();
+           // Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            using (Stream dataStream = response.GetResponseStream())
             {
-                System.Console.WriteLine(rivi);
-                summa += int.Parse(rivi);
-                counter++;
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                int indexOfTemp = responseFromServer.IndexOf("temp");
+                string ans = responseFromServer.Substring(indexOfTemp, 10);
+                string temp = ans.Substring(6,4);
+               
+                //string name = (string)o["temp"];
+                Console.WriteLine($"Lämpötila Tampereella on {temp} astetta.");
             }
 
-            file.Close();
-            System.Console.WriteLine("There were {0} lines.", counter);
-            System.Console.WriteLine("Summa on: {0}.", summa);
+            // Close the response.
+            response.Close();
+            
         }
     }
 }
