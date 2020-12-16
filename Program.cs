189bf4ae4588e4e5299223fde318705dfa0ee60a
Ilari Lehtinen <ilari.m.lehtinen@gmail.com>
2020-12-16 09:59:03 +0200

    First commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            int counter = 0;
+            int summa = 0;
+            string line;
             Console.WriteLine("Hello World!");
+            System.IO.StreamReader file = new System.IO.StreamReader("test.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                summa += int.Parse(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            System.Console.WriteLine("Summa on: {0}.", summa);
         }
     }
 }
